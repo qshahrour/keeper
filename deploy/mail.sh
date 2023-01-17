8336fca65772d7df8a1c5fbe4801591eadfc8633
qshahrour <qshahrour88@gmail.com>
2023-01-18 02:49:03 +0300

    Https GET

--- deploy/mail.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# This Script Runs Ansible Mail Module To Send an Email.
-# Ansible Script  :  mail.yml           #
-# Ansible Path    :  ~/keeper/          #
-# Ansible hosts   :  ansible.inventory  #
-
-/usr/bin/ansible-playbook -i keeper/ansible.inventory keeper/mail.yml
